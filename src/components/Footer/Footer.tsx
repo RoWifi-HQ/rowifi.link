2ca1ade6f1240dc3789af431bd9562c4c588ae39
AsianIntel <gautam.abhyankar@gmail.com>
2021-08-13 02:18:41 +0530

    Fix design for mobile

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer className="bg-dashboard-dark">
-            <div className="container mx-auto flex px-16 py-16">
-                <div className="flex flex-col">
+            <div className="container mx-auto flex flex-col md:flex-row px-16 py-16 text-center md:text-left">
+                <div className="flex flex-col items-center md:items-start">
                     <div className="flex flex-row items-center">
                         <Image src="/rowifi.png" alt="RoWifi Logo" height={50} width={50} />
                         <span className="text-xl font-semibold ml-3">RoWifi</span>
                     </div>
                     <span className="text-sm text-gray-300 mt-4">© 2021 RoWifi — @AsianIntel</span>
                 </div>
-                <div className="flex ml-auto space-x-16">
+                <div className="flex flex-col md:flex-row md:ml-auto md:space-x-16 space-y-8 md:space-y-0 text-center md:text-left mt-8 md:mt-0">
                     <div>
                         <span>Legal</span>
                         <nav className="list-none mt-4 space-y-2">
@@ -21,7 +21,7 @@ export default function Footer() {
                         </nav>
                     </div>
                     <div>
-                        <span className="">Social</span>
+                        <span>Social</span>
                         <nav className="list-none mt-4 space-y-2">
                             <li className="text-sm text-gray-300 hover:text-link-dark">
                                 <a href="https://twitter.com/rowifibot" target="_blank" rel="noreferrer">Twitter</a>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>RoWifi - The 2nd Gen Roblox-Discord Verification Bot</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
