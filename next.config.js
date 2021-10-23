26091034baeac8244d52abe7c2cfdfbc9017bd88
AsianIntel <gautam.abhyankar@gmail.com>
2021-10-23 17:46:07 +0530

    Add ids to doc headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const gfm = require("remark-gfm");
+const slug = require("remark-slug");
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [gfm],
+    remarkPlugins: [gfm, slug],
   },
 });
 
