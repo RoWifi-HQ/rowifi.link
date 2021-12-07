0162b381e6321744698b5d7ef778a07e87d1c081
AsianIntel <gautam.abhyankar@gmail.com>
2021-12-08 00:47:39 +0530

    Tweak unit css

--- src/components/Miscellanous/AdBanner.tsx
+++ src/components/Miscellanous/AdBanner.tsx
@@ -1,5 +1,13 @@
 import { useEffect } from "react";
 
+const style = {
+    display: "block",
+    height: "128px",
+    "@media (min-width: 1024px)": {
+        width: "1024px",
+    }
+};
+
 export default function AdBanner() {
     useEffect(() => {
         try {
@@ -12,8 +20,8 @@ export default function AdBanner() {
     }, []);
 
     return (
-        <ins className="adsbygoogle container"
-            style={{ display: "block" }}
+        <ins className="adsbygoogle"
+            style={style}
             data-ad-client="ca-pub-1197269638005739"
             data-ad-slot="7242297329"
             data-ad-format="auto" />
